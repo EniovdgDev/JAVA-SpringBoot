675d9eb88396c03ef569fe48c6df1d08bfca9186
eniov <eniovdg@18>
2023-06-29 18:11:28 -0300

    Spring Init para criar projeto. Maven + Java 20 + SB 3.1.1. devtools/lombok/springweb

---
+++ src/test/java/enio/vieira/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package enio.vieira.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
