fa85cc461540185233b37da5a2a10000bd91a00d
eniov <eniovdg@18>
2023-07-03 14:08:00 -0300

    Implementação do método Get

--- src/main/java/enio/vieira/api/controller/MedicoController.java
+++ src/main/java/enio/vieira/api/controller/MedicoController.java
@@ -1,16 +1,17 @@
-package enio.vieira.api;
+package enio.vieira.api.controller;
 
 import enio.vieira.api.medico.DadosCadastroMedico;
+import enio.vieira.api.medico.DadosListagemMedico;
 import enio.vieira.api.medico.Medico;
 import enio.vieira.api.medico.MedicoRepository;
 
 import jakarta.validation.Valid;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /*
  Usa-se o requestMapping para dizer qual a rota desse controller
@@ -22,12 +23,18 @@ public class MedicoController {
     @Autowired // serve para injetar dependencias
     private MedicoRepository repository; // serve para comunicar com o banco
 
-    //tipo de requisição(Post) - @reqBody serve para dizer que o dado vem do body da request
-    @PostMapping
+    @PostMapping //tipo de requisição(Post) - @reqBody serve para dizer que o dado vem do body da request
     @Transactional // escrever no banco
     public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados){ // @Valid para ver se o DTO veio correto
         //Receber o DTO e transforma no Medico usando o contrutor
         repository.save(new Medico(dados));
+    }
+
+
+    @GetMapping //tipo de requisição(get)
+    public List<DadosListagemMedico> listar(){
+
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList(); // transforma medico em DadosListagemMedico
     }
 
 }
